805708925aec9d7265972aeeeece86c6bfcd5da2
yamineHjr <yasmine.alnajar@gmail.com>
2024-08-13 12:16:12 +0300

    change client url

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import {Server} from 'socket.io';
 
 const io = new Server({
     cors: {
-        origin: 'http://localhost:5173',
+        // origin: 'http://localhost:5173',
+        origin: 'https://estate-client-ym15.onrender.com',
     }
 })
 
